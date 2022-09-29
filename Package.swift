b372057570940558d55ba6d5c1e5eb2623b983b5
jjpb <punongbayanjonathan@peatix.com>
2022-09-29 16:07:14 +0800

    Fix crash with static libraries

--- Package.swift
+++ Package.swift
@@ -1,24 +1,41 @@
-// swift-tools-version:5.0
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "CardIO",
-    platforms: [.iOS(.v9)],
+    platforms: [.iOS(.v12)],
     products: [
         .library(
             name: "CardIO",
-            targets: ["CardIO"]
+            targets: ["CardIO", "libCardIO", "libopencv_core", "libopencv_imgproc"]
         )
     ],
     targets: [
         .target(
             name: "CardIO",
             path: "CardIO",
-            exclude: ["SampleApp", "SampleApp-Swift"],
+            exclude: ["../SampleApp", "../SampleApp-Swift"],
             publicHeadersPath: ".",
-            cSettings: [
-                .headerSearchPath("CardIO")
+            linkerSettings: [
+              .linkedLibrary("c++")
             ]
+        ),
+        .binaryTarget(
+            name: "libCardIO",
+            path: "Frameworks/libCardIO.xcframework"
+        ),
+        .binaryTarget(
+            name: "libopencv_core",
+            path: "Frameworks/libopencv_core.xcframework"
+        ),
+        .binaryTarget(
+            name: "libopencv_imgproc",
+            path: "Frameworks/libopencv_imgproc.xcframework"
         )
     ]
 )
+
+
+
